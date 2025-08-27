243ab90a835da3a52252092462a455e023237437
Universe <universe@grida.co>
2025-08-27 22:35:58 +0900

    cors allow

--- www/next.config.ts
+++ www/next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /** @type {import('next').NextConfig} */
+
+  async headers() {
+    return [
+      {
+        // match all files under /public
+        source: "/:all*(svg|jpg|png|woff|woff2|ttf|otf|json)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // or "https://grida.co" (let's decide ths later)
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, OPTIONS",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
